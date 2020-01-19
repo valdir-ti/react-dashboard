8a8bfd960beacfcab976fd2c626df5879c26aa0d
valdir-ti <valdir.ti@gmail.com>
2020-01-19 18:18:52 -0300

    Configurações básicas iniciais de um projeto

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Users from './pages/Users';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Main}></Route>
+                <Route path="/users" component={Users}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
