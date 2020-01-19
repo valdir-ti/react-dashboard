1fe7a1d93888b3ca6f47996580109b117b3f57b0
valdir-ti <valdir.ti@gmail.com>
2020-01-19 19:04:00 -0300

    Estilos Globais

--- src/pages/Users/styles.js
+++ src/pages/Users/styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const UserTitle = styled.h1`
     font-size: 22px;
-    color: #7159c1;
+    color: #FFF;
     font-family: Arial, Helvetica, sans-serif;
 `;
